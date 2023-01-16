59d161701dc28e4ab06bfabc774946a3dfa00baa
Jayas Desai <jayas@Jayass-MacBook-Air.local>
2023-01-16 19:54:45 +0530

    updated

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
-import org.testng.log4testng.Logger;
-
 import com.crm.qa.utli.TestUtil;
 import com.crm.qa.utli.WebEventListener;
 
